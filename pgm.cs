a267ec60d303fea0726ba1af905a64f36252b2a1
muralimd07 <60916429+muralimd07@users.noreply.github.com>
2020-12-22 02:23:40 -0800

    added cs file

---
+++ pgm.cs
@@ -0,0 +1,15 @@
+using System;
+namespace calculator1App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public int add(int a,int b)
+        {
+            return a+b;
+        }
+    }
+}
